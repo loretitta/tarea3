f73adf4a93ca46503afde7480f80c97b71d7e78a
Loreta <susses.katzchen@gmail.com>
2013-04-11 23:33:52 -0300

    main

--- connPgSQL.cpp
+++ connPgSQL.cpp
@@ -13,3 +13,42 @@ char *dataBase = "iswdb";
 char *user = "isw";
 char *passwd = "isw";
 
+int main(int argc, char * argv[])
+{
+    int i;
+    cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
+
+    if (PQstatus(cnn) != CONNECTION_BAD) 
+    {
+        cout << "Conectado a PostgreSQL!" << endl;
+        
+        result = PQexec(cnn, "SELECT * FROM cursos");
+
+        if (result != NULL) 
+        {
+  result =PQexec(cnn,"SELECT distinct docente_id FROM cursos");
+  int tuplas = PQntuples(result);
+         int campos = PQnfields(result);
+  for (i=0; i<tuplas; i++) {
+                for (int j=0; j<campos; j++) {
+      cout <<"ID Docente : ";
+                    cout << PQgetvalue(result,i,j) << " | ";
+      
+                }
+                cout << endl;
+                }
+
+        // liberar la memoria
+        PQclear(result);
+        }
+    }
+ 
+    else {
+        cout << "Error de conexion" << endl;
+        return 0;
+    }
+
+    PQfinish(cnn);
+
+    return 0;
+}
