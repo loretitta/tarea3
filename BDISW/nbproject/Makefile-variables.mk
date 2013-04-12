ec2b5a61a4f71cd6b139cbabfd64f10aeb4ace18
lain21 <batusay-16@hotmail.com>
2013-04-12 00:02:35 -0400

    integrantes

--- BDISW/nbproject/Makefile-variables.mk
+++ BDISW/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=bdisw
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/bdisw
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/bdisw
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=bdisw.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/bdisw.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/bdisw.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=bdisw
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/bdisw
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/bdisw
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=bdisw.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/bdisw.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/bdisw.tar
 #
 # include compiler specific variables
 #
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 tarea3
 ======
+-Mario Ibarra T.
+-Loreta Astudillo O.
+-Abraham Muñoz S.
